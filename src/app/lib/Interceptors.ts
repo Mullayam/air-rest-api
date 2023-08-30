5b317b016c5cc89cc2feecd7efadf7fb16b8c788
Mulayam S <mullayam06@gmail.com>
2023-08-30 19:14:34 +0530

    preapraring for interceptor

--- src/app/lib/Interceptors.ts
+++ src/app/lib/Interceptors.ts
@@ -1,5 +1,16 @@
-import {Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+ 
+import { Logging } from "../../logs/index.js";
+
 
 class Interceptor {
-    constructor(public req: Request, public res: Response, public next: NextFunction) { }
+    constructor(public req: Request, public res: Response, public next: NextFunction) {
+
+    }
+    useInterceptor() {
+        return (function (req: Request, res: Response) {
+ 
+        })
+
+    }
 }
