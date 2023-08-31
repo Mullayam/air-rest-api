19460021162215b8842ec3d2ed302f1096d58a63
Mulayam S <mullayam06@gmail.com>
2023-08-31 22:41:23 +0530

    add landing page

---
+++ pages/my-app/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],  
+  variants: {
+    extend: {},
+  },
+  theme: {
+    extend: {      
+      colors: {
+        gray: {
+          100: "#FBFBFB",
+          200: "#EAEAEA",
+          300: "#DFDFDF",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#333333",
+          900: "#191919",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
